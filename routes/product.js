ceae163ccf71d9369077f6232049af690b3f643f
Pedro Rios <pedroriosg@uc.cl>
2023-09-17 20:11:52 -0300

    feat: delete ok and tested

--- routes/product.js
+++ routes/product.js
@@ -4,7 +4,23 @@ const { Product } = require('../models');
 
 // Otras rutas...
 
-console.log('Ruta de productos cargada');
+const authenticateToken = (req, res, next) => {
+  // Token predefinido (puedes cambiarlo según tus necesidades)
+  const predefinedToken = 'iicapit2';
+
+  // Obtén el token proporcionado en la solicitud (por ejemplo, en el encabezado "Authorization")
+  const tokenProvided = req.headers.authorization;
+
+  // Verifica si el token proporcionado coincide con el token predefinido
+  if (tokenProvided === `Bearer ${predefinedToken}`) {
+    // Si el token coincide, permite que la solicitud continúe
+    next();
+  } else {
+    // Si el token no coincide, responde con un error 401 (No autorizado)
+    res.status(401).json({ error: 'Token no válido' });
+  }
+};
+
 
 // Ruta para obtener todos los productos
 router.get('/', async (req, res) => {
@@ -50,6 +66,49 @@ router.get('/:category/:id', async (req, res) => {
     res.status(500).json({ error: 'Error al obtener el producto' });
   }
 });
+
+router.post('/', authenticateToken, async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al crear el producto' });
+  }
+});
+
+router.put('/:id', authenticateToken, async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findByPk(productId);
+    if (product) {
+      const updatedProduct = await product.update(req.body);
+      res.json(updatedProduct);
+    } else {
+      res.status(404).json({ error: 'Producto no encontrado' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al actualizar el producto' });
+  }
+});
+
+router.delete('/:id', authenticateToken, async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findByPk(productId);
+    if (product) {
+      await product.destroy();
+      res.json({ message: 'Producto eliminado' });
+    } else {
+      res.status(404).json({ error: 'Producto no encontrado' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al eliminar el producto' });
+  }
+});
+
 
 
 module.exports = router;
