4b6d5cde9d3ea2c9b09856832dcd807f4142de17
Pedro Rios <pedroriosg@uc.cl>
2023-09-19 23:55:35 -0300

    chron job seeds

---
+++ seed-scheduler.js
@@ -0,0 +1,20 @@
+const cron = require('node-cron');
+const { exec } = require('child_process');
+
+// Programa la tarea para ejecutarse todos los días a las 23:30
+cron.schedule('55 23 * * *', () => {
+   console.log('Iniciando tarea programada...');
+   
+   // Ejecuta los comandos
+   exec('npx sequelize-cli db:drop && npx sequelize-cli db:create && npx sequelize-cli db:migrate && npx sequelize-cli db:seed:all', (error, stdout, stderr) => {
+     if (error) {
+       console.error(`Error al ejecutar los comandos: ${error}`);
+       return;
+     }
+
+     console.log(`stdout: ${stdout}`);
+     console.error(`stderr: ${stderr}`);
+
+     console.log('Tarea programada completada exitosamente.');
+   });
+});
