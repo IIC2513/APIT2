2647c443b12866d78b68aac8eb224c2483077b56
Pedro Rios <pedroriosg@uc.cl>
2023-09-20 00:09:00 -0300

    feat: seeds 60

---
+++ Readme.md
@@ -0,0 +1,9 @@
+## Comandos chron-job
+
+`pm2 start seed-scheduler.js`
+
+
+`pm2 list`
+
+
+`pm2 stop seed-scheduler`
--- seeders/20230917210446-product.js
+++ seeders/20230917210446-product.js
@@ -1,46 +1,33 @@
 'use strict';
+const Chance = require('chance');
+const chance = new Chance();
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // Inserta datos en la tabla Product
-    await queryInterface.bulkInsert('Products', [
-      {
-        name: 'Producto 1',
-        description: 'Descripción del Producto 1',
-        stock: 10,
-        price: 19.99,
-        category: 'Electrónica',
+    const products = [];
+    
+    // Genera 60 productos aleatorios
+    for (let i = 0; i < 60; i++) {
+      const productName = chance.word();
+      const productDescription = chance.sentence();
+      const productStock = chance.integer({ min: 1, max: 50 });
+      const productPrice = chance.floating({ min: 10, max: 1000, fixed: 2 });
+      const productCategory = chance.pickone(['Electrónica', 'Ropa', 'Hogar', 'Deportes']);
+
+      products.push({
+        name: productName,
+        description: productDescription,
+        stock: productStock,
+        price: productPrice,
+        category: productCategory,
         createdAt: new Date(),
         updatedAt: new Date(),
-      },
-      {
-        name: 'Producto 2',
-        description: 'Descripción del Producto 2',
-        stock: 15,
-        price: 29.99,
-        category: 'Ropa',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Producto 3',
-        description: 'Descripción del Producto 3',
-        stock: 5,
-        price: 9.99,
-        category: 'Hogar',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        name: 'Producto 4',
-        description: 'Descripción del Producto 4',
-        stock: 20,
-        price: 49.99,
-        category: 'Electrónica',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ], {});
+      });
+    }
+
+    await queryInterface.bulkInsert('Products', products, {});
+
   },
 
   down: async (queryInterface, Sequelize) => {
